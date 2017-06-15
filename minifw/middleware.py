be7733f38191f6a24be70ae20c635fa2e07b6929
resolvewang <1796246076@qq.com>
2017-06-15 21:56:23 +0800

    add request middleware

---
+++ minifw/middleware.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from aiohttp import web
+
+
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: {} {}'.format(request.method, request.path))
+        return await handler(request)
+    return logger
+
+async def response_factory(app, handler):
+    async def response(request):
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False,
+                                                    default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and 100 <= r < 600:
+            return web.Response(r)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and 100 <= t < 600:
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+    return response
