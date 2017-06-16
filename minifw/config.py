148bef0f4c431c805f3d2016fb1a9aa5e1611cce
resolvewang <1796246076@qq.com>
2017-06-16 21:10:43 +0800

    add config default and override

---
+++ minifw/config.py
@@ -0,0 +1,48 @@
+from minifw import config_default
+
+
+class Dict(dict):
+    def __init__(self, names=(), values=(), **kwargs):
+        super().__init__(**kwargs)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError('Dict object has no attribute {}'.format(key))
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+def merge(defaults, override):
+    r = dict()
+    for k, v in defaults.items():
+        if k in override:
+            if isinstance(v, dict):
+                r[k] = merge(v, override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
+
+
+def to_dict(d):
+    D = Dict()
+    for k, v in d.items():
+        D[k] = to_dict(v) if isinstance(v, dict) else v
+    return D
+
+configs = config_default.configs
+
+try:
+    from minifw import config_override
+except ImportError:
+    pass
+else:
+    configs = merge(configs, config_override.configs)
+
+configs = to_dict(configs)
