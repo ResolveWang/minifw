c0ca0417fb71f0efc3f2defb6d2819315a179013
resolvewang <1796246076@qq.com>
2017-08-15 21:46:46 +0800

    add test for db

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,6 +2,7 @@ import asyncio
 import unittest
 import aiomysql
 from minifw.db import base_db
+from examples.orm_opera import User
 
 
 class TestDB(unittest.TestCase):
@@ -16,9 +17,24 @@ class TestDB(unittest.TestCase):
         rs = self.loop.run_until_complete(base_db.select(self.pool, sql, args=(1,), size=1))
         self.assertEqual(len(rs), 1)
 
-    def test_insert(self):
+    def test_execute(self):
         sql = 'insert into `minifw` (`name`) values (?)'
-        rs = self.loop.run_until_complete(base_db.insert(self.pool, sql, args=('test_val',)))
+        rs = self.loop.run_until_complete(base_db.execute(self.pool, sql, args=('test_val',)))
+        self.assertEqual(rs, 1)
+
+    def test_save(self):
+        user = User(id=100, name='hello')
+        rs = self.loop.run_until_complete(user.save(self.pool))
+        self.assertEqual(rs, 1)
+
+    def test_find(self):
+        user = self.loop.run_until_complete(User.find(self.pool, 100))
+        self.assertEqual(user.name, 'hello')
+
+    def test_update(self):
+        user = self.loop.run_until_complete(User.find(self.pool, 100))
+        user.name = 'test'
+        rs = self.loop.run_until_complete(user.update(self.pool))
         self.assertEqual(rs, 1)
 
     def tearDown(self):
